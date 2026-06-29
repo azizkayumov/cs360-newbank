2b9ed11942b472bd866b0ea235ef851c73528146
Azizbek1308 <a.ergashov@newuu.uz>
2026-06-29 17:16:44 +0500

    adding new end point cards/Visa(Uzcard,Humo)

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('cards/', views.cards, name='cards'),
+    path('cards/<str:card_type>/', views.card_detail, name='card_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,10 @@ def index(request):
 
 def cards(request):
     return render(request, 'home/index.html')
+
+def card_detail(request, card_type):
+    
+    context = {
+        'card_name': card_type,
+    }
+    return render(request, 'home/card_detail.html', context)
