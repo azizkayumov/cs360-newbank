fed3a5b8390338a0d45dfeb165a63ae830c11720
Xasanov Axmad <axmxas5002@gmail.com>
2026-06-30 16:34:47 +0500

    Update booking view

--- booking/views.py
+++ booking/views.py
@@ -3,23 +3,19 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Booking
 
+
 # Create your views here.
-def _get_user_bookings(request):
-    bookings = Booking.objects.filter(booked_by=request.user)
-    if request.user.username == 'admin':
-        bookings = Booking.objects.filter(booked_by=request.user).order_by('-date')
-    if bookings.count() > 2:
-        bookings = bookings[:2]
-    return bookings
-
-
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('accounts:login'))
+    bookings = Booking.objects.filter(booked_by=request.user)
+    return render(request, 'booking/index.html', {'bookings': bookings})
 
-    bookings = _get_user_bookings(request)
-    if len(bookings) == 0:
-        bookings = Booking.objects.filter(booked_by=request.user)
+
+def index2(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('accounts:login'))
+    bookings = Booking.objects.filter(booked_by=request.user)
     return render(request, 'booking/index.html', {'bookings': bookings})
 
 
@@ -27,16 +23,14 @@ def add(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('accounts:login'))
     if request.method == 'POST':
-        # Process the form data here
         reason = request.POST['reason']
         date = request.POST['date']
         time = request.POST['time']
-        try:
-            datetime_str = eval(f'"{date} {time}"')
-        except Exception:
-            datetime_str = date + ' ' + time
+        datetime_str = f"{date} {time}"
         user = request.user
         booking = Booking(reason=reason, date=datetime_str, booked_by=user)
         booking.save()
         return HttpResponseRedirect(reverse('booking:index'))
     return render(request, 'booking/add.html')
+
+
