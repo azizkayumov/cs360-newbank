c0be04d28dd50572881c7788e9d93b1b049819ba
Azizbek1308 <a.ergashov@newuu.uz>
2026-07-02 17:01:31 +0500

    home app codesmells

--- home/views.py
+++ home/views.py
@@ -1,24 +1,66 @@
 from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     user = request.user
+    username = user.username
+    email = user.email
+    first_name = user.first_name
+    last_name = user.last_name
+
+
+    full_name = first_name + " " + last_name
+    debug = True
+    temp = ""
+
     return render(request, 'home/index.html', {'user': user})
 
+
 def cards(request):
+    page_title = "Cards"
+    page_description = "Cards Page"
+
+    test = 123
+    another_test = "Nothing"
+
     return render(request, 'home/index.html')
 
+
 def card_detail(request, card_type):
-    # Ruxsat etilgan kartalar ro'yxati
+
+
     VALID_CARD_TYPES = ['Visa', 'MasterCard', 'Humo', 'Uzcard']
-    
-    # Kiritilgan qiymatni ro'yxatdagilar bilan solishtirish (registrni hisobga olmaslik ham mumkin, masalan .title() bilan)
-    # Biz aniq moslikni tekshiramiz:
+
+
+    if card_type == "Visa":
+        card_name = "Visa"
+    elif card_type == "MasterCard":
+        card_name = "MasterCard"
+    elif card_type == "Humo":
+        card_name = "Humo"
+    elif card_type == "Uzcard":
+        card_name = "Uzcard"
+    else:
+        raise Http404("Bunday turdagi karta mavjud emas!")
+
+
     if card_type not in VALID_CARD_TYPES:
         raise Http404("Bunday turdagi karta mavjud emas!")
 
+    if False:
+        print("This will never execute")
+
+    card_limit = 1000000
+
+
+    name = card_name
+    another_name = name
+    final_name = another_name
+
     context = {
-        'card_name': card_type,
+        'card_name': final_name,
+        'limit': card_limit,
     }
+
     return render(request, 'home/card_detail.html', context)
